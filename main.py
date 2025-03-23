bf745d25908dee1bb776c120894aafab3aab947d
PaulL7 <47078682+Paulsmek@users.noreply.github.com>
2025-03-23 18:00:55 +0200

    Updated code that creates output parquet files
    - Function for normalisation
    - Helper function for converting fields to string before creating parquet files
    - Improved time for non dups and dups handling and concatenation
    - Two outputs, one with normalised fields and one with none

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ def normalize_text(text):
     no_punc_string = re.sub(r'[^\w\s]','', lower_string)
     return no_punc_string
 
+def add_normalized_fields(df):
+    df['normalized_name'] = df['product_name'].apply(normalize_text)
+    df['normalized_title'] = df['product_title'].apply(normalize_text)
+    df['normalized_brand'] = df['brand'].apply(normalize_text)
+    return df
+
 # Function to print duplicates for verification
 def print_duplicates(data_group):
     output_file = 'potential_duplicates.txt'
@@ -23,7 +29,7 @@ def print_duplicates(data_group):
         # Show up to 10 duplicates
         for name, group in list(data_group)[0:10]:
             f.write(f" Group: '{name}' —> {len(group)} entries\n")
-            f.write(group[['applicability','product_name', 'product_title','brand', 'price', 'description']].to_string(index=False))
+            f.write(group[['product_name', 'product_title','brand', 'price', 'description']].to_string(index=False))
             f.write("\n\n" + "-"*100 + "\n\n")
 
 def merge_group(group):
@@ -36,7 +42,7 @@ def merge_group(group):
         # Flatten lists to strings
         flat_vals = []
         for val in series:
-            if isinstance(val, list):
+            if isinstance(val, (list, dict)):
                 flat_vals.append(str(val))
             else:
                 flat_vals.append(str(val).strip())
@@ -53,22 +59,39 @@ def merge_group(group):
 
     return pd.Series(merged)
 
+# Function to convert lists/dicts to strings before parquet file
+def lists_or_dicts_to_string(df):
+    for col in df.columns:
+        if df[col].apply(lambda x: isinstance(x, (list, dict))).any():
+            df[col] = df[col].apply(lambda x: str(x) if isinstance(x, (list, dict)) else x)
+    return df
 
 # Add normalized name column, and also normalised brand name
-data['normalized_name'] = data['product_name'].apply(normalize_text)
-data['normalized_title'] = data['product_title'].apply(normalize_text)
-data['normalized_brand'] = data['brand'].apply(normalize_text)
+data = add_normalized_fields(data)
 
 # Group by normalized main columns and filter to get dups
 grouped = data.groupby(['normalized_name','normalized_title', 'normalized_brand'])
 filtered_groups = [(key, group) for key, group in grouped if len(group) > 1]
 
-# 'grouped' for complete data, 'filtered_groups' to check merger [_______________]
-merged_entries = pd.DataFrame([merge_group(group) for _, group in filtered_groups])
-merged_entries['normalized_name'] = merged_entries['product_name'].apply(normalize_text)
-merged_entries['normalized_title'] = merged_entries['product_title'].apply(normalize_text)
-merged_entries['normalized_brand'] = merged_entries['brand'].apply(normalize_text)
+# Get non dups and concatenate them
+non_duplicate_groups = [group for _, group in grouped if len(group) == 1]
+non_duplicates = pd.concat(non_duplicate_groups)
 
-grouped2 = merged_entries.groupby(['normalized_name', 'normalized_title', 'normalized_brand'])
+# Merge all groups
+merged_entries = pd.concat([merge_group(group) for _, group in filtered_groups])
+merged_entries = add_normalized_fields(merged_entries)
 
-print_duplicates(grouped2)
+data_post_merge = pd.concat([non_duplicates, merged_entries], ignore_index=True)
+
+# Verify if there are dups in final_data
+grouped2 = data_post_merge.groupby(['normalized_name', 'normalized_title', 'normalized_brand'])
+filtered2_groups = [(key, group) for key, group in grouped2 if len(group) > 1]
+print_duplicates(filtered2_groups)
+
+# Parquet file with normalised fields
+final_data_stringed = lists_or_dicts_to_string(data_post_merge)
+final_data_stringed.to_parquet('final_deduplicated.parquet', index=False)
+
+# Parquet file with no normalised fields
+final_data_nonormal = final_data_stringed.drop(columns=['normalized_name', 'normalized_title', 'normalized_brand'])
+final_data_stringed.to_parquet('final_clean_deduplicated.parquet', index=False)
